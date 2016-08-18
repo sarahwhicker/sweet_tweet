f5a75f67c90415f6d68a13fb8772860be73ecd90
SARAH WHICKER <slickwhick@gmail.com>
2016-08-17 20:44:53 -0500

    Set up CRUD routes for tweets and root

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
 
   get 'tweets/_form'
 
+  resources :tweets
+
   devise_for :users
+
+  root 'tweets#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
